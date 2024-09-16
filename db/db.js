e6813946153f6709e6c6ffe12cb83a9cd043ad47
aakashhari16249865 <aakashhari729865@gmail.com>
2024-09-16 19:12:09 +0530

    new

--- db/db.js
+++ db/db.js
@@ -1,6 +1,14 @@
+// import mongoose from "mongoose";
+
+// const con=mongoose.createConnection('mongodb://localhost:27017/lsp');
+// con.once('open',()=>console.log("database connected"));
+// con.on('error',(err)=>console.log(`database not connect:${err.message}`));
+
+// export default con
+
 import mongoose from "mongoose";
 
-const con=mongoose.createConnection('mongodb://localhost:27017/lsp');
+const con=mongoose.createConnection('mongodb+srv://aakashofficial2005:GAcJIEDKeXswQ8A6@cluster0.6sv3s.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0');
 con.once('open',()=>console.log("database connected"));
 con.on('error',(err)=>console.log(`database not connect:${err.message}`));
 
