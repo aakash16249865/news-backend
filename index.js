b8db1846f57b82ac51d5fd402e7dfea23a0e8724
aakashhari16249865 <aakashhari729865@gmail.com>
2024-09-16 19:51:51 +0530

    allow all ip

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import userregister from './controllers/register.controller.js';
+// import logiregister from './controllers/login.controller.js';
+// import apiRouter from './routes/apiRoutes.js';
+
+// dotenv.config();
+
+// const app= express();
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}));
+// app.use(cors({
+//     origin:['http://localhost:5173']
+// }))
+
+
+// app.post('/user/userregister', userregister)
+// app.post('/user/loginuser', logiregister)
+// app.get('/users',apiRouter);
+// app.get('/',(req,res)=>res.send("hello world"));
+
+// const port = process.env.PORT || 3001 ;
+
+// app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
@@ -8,20 +36,25 @@ import apiRouter from './routes/apiRoutes.js';
 
 dotenv.config();
 
-const app= express();
+const app = express();
 
+// Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Allow all IPs and origins to access the server
 app.use(cors({
-    origin:['http://localhost:5173']
-}))
+    origin: true,  // This allows all origins (effectively all IPs)
+    credentials: true // Allows credentials to be shared if necessary
+}));
 
+// Routes
+app.post('/user/userregister', userregister);
+app.post('/user/loginuser', logiregister);
+app.get('/users', apiRouter);
+app.get('/', (req, res) => res.send("Hello, World"));
 
-app.post('/user/userregister', userregister)
-app.post('/user/loginuser', logiregister)
-app.get('/users',apiRouter);
-app.get('/',(req,res)=>res.send("hello world"));
+// Port setup
+const port = process.env.PORT || 3001;
 
-const port = process.env.PORT || 3001 ;
-
-app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
